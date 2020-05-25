cfe61ac4d668428ea72f91392340981bc7f1ba45
Ramis <i.ramis95@gmail.com>
2020-05-25 13:57:25 +0300

    'removedgitignored_files'

--- application/config/DB_config.php
+++
@@ -1,5 +0,0 @@
-<?
-
-define('DB_HOST', 'localhost1');
-define('DB_USER', 'localhost5');
-define('DB_PASS', 'localhost1');
--- application/config/Rabbit_config.php
+++
@@ -1,6 +0,0 @@
-<?
-
-define('QUEU_HOST', 'localhost');
-define('QUEU_PORT', 5672);
-define('QUEU_USER', 'user1');
-define('QUEU_PASSWORD', '123');
